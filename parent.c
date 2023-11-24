6339fafe8d8f1b757355b77bf126126c956f765b
Oumb2021 <baarryoumar@gmail.com>
2023-11-23 22:12:29 -0500

    finalized

--- parent.c
+++ parent.c
@@ -8,30 +8,38 @@
 
 int main(int argc, char *argv[])
 {
-  pid_t pid;
+  pid_t pid; /* process id */
 
   for (int i = 0; i < 3; ++i)
   {
     pid = fork();
 
+    // check for errors in the creation of the process
     if (pid == -1)
     {
       perror("Process creation failed");
       exit(EXIT_FAILURE);
     }
+
     else if (pid == 0)
     {
       // Child process
       switch (i)
       {
+
+      // Child process 1
       case 0:
         execl("./child1", "child1", argv[1], argv[2], NULL);
         perror("Error in execl for child1");
         exit(EXIT_FAILURE);
+
+      // Child process 2
       case 1:
         execl("./child2", "child2", argv[3], NULL);
         perror("Error in execl for child2");
         exit(EXIT_FAILURE);
+
+      // Child process 3
       case 2:
         execlp("./child3", "child3", NULL);
         perror("Error in execlp for child3");
@@ -45,8 +53,4 @@ int main(int argc, char *argv[])
   {
     wait(NULL);
   }
-
-  printf("Parent process exiting.\n");
-
-  exit(EXIT_SUCCESS);
 }
